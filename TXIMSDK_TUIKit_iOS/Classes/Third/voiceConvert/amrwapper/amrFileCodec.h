35976da9cba08d6386c5dc7d1c53650e03707675
Miles <liyida188@163.com>
2023-04-11 12:24:31 +0800

    update

--- TXIMSDK_TUIKit_iOS/Classes/Third/voiceConvert/amrwapper/amrFileCodec.h
+++ TXIMSDK_TUIKit_iOS/Classes/Third/voiceConvert/amrwapper/amrFileCodec.h
@@ -7,14 +7,15 @@
 //
 #ifndef amrFileCodec_h
 #define amrFileCodec_h
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include "interf_dec.h"
 #include "interf_enc.h"
+#import <Foundation/Foundation.h>
 
 #define AMR_MAGIC_NUMBER "#!AMR\n"
-#define MP3_MAGIC_NUMBER "ID3"
 
 #define PCM_FRAME_SIZE 160 // 8khz 8000*0.02=160
 #define MAX_AMR_FRAME_SIZE 32
@@ -22,57 +23,60 @@
 
 typedef struct
 {
-    char chChunkID[4];
-    int nChunkSize;
-}EM_XCHUNKHEADER;
+	char chChunkID[4];
+	int nChunkSize;
+}XCHUNKHEADER;
 
 typedef struct
 {
-    short nFormatTag;
-    short nChannels;
-    int nSamplesPerSec;
-    int nAvgBytesPerSec;
-    short nBlockAlign;
-    short nBitsPerSample;
-}EM_WAVEFORMAT;
+	short nFormatTag;
+	short nChannels;
+	int nSamplesPerSec;
+	int nAvgBytesPerSec;
+	short nBlockAlign;
+	short nBitsPerSample;
+}WAVEFORMAT;
 
 typedef struct
 {
-    short nFormatTag;
-    short nChannels;
-    int nSamplesPerSec;
-    int nAvgBytesPerSec;
-    short nBlockAlign;
-    short nBitsPerSample;
-    short nExSize;
-}EM_WAVEFORMATX;
+	short nFormatTag;
+	short nChannels;
+	int nSamplesPerSec;
+	int nAvgBytesPerSec;
+	short nBlockAlign;
+	short nBitsPerSample;
+	short nExSize;
+}WAVEFORMATX;
 
 typedef struct
 {
-    char chRiffID[4];
-    int nRiffSize;
-    char chRiffFormat[4];
-}EM_RIFFHEADER;
+	char chRiffID[4];
+	int nRiffSize;
+	char chRiffFormat[4];
+}RIFFHEADER;
 
 typedef struct
 {
-    char chFmtID[4];
-    int nFmtSize;
-    EM_WAVEFORMAT wf;
-}EM_FMTBLOCK;
+	char chFmtID[4];
+	int nFmtSize;
+	WAVEFORMAT wf;
+}FMTBLOCK;
 
-// WAVE audio processing frequency is 8khz
-// audio processing unit = 8000*0.02 = 160 (decided by audio processing frequency)
-// audio channels 1 : 160
+// WAVE音频采样频率是8khz 
+// 音频样本单元数 = 8000*0.02 = 160 (由采样频率决定)
+// 声道数 1 : 160
 //        2 : 160*2 = 320
-// bps decides the size of processing sample
-// bps = 8 --> 8 bits
-//       16 --> 16 bit
-int EM_EncodeWAVEFileToAMRFile(const char* pchWAVEFilename, const char* pchAMRFileName, int nChannels, int nBitsPerSample);
+// bps决定样本(sample)大小
+// bps = 8 --> 8位 unsigned char
+//       16 --> 16位 unsigned short
+int EncodeWAVEFileToAMRFile(const char* pchWAVEFilename, const char* pchAMRFileName, int nChannels, int nBitsPerSample);
 
-int EM_DecodeAMRFileToWAVEFile(const char* pchAMRFileName, const char* pchWAVEFilename);
 
-int isMP3File(const char *filePath);
+// 将AMR文件解码成WAVE文件
+int DecodeAMRFileToWAVEFile(const char* pchAMRFileName, const char* pchWAVEFilename);
 
-int isAMRFile(const char *filePath);
+
+NSData* DecodeAMRToWAVE(NSData* data);
+NSData* EncodeWAVEToAMR(NSData* data, int nChannels, int nBitsPerSample);
+
 #endif
